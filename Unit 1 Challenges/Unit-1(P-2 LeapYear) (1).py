5a46987a73e846c68ed66333bc4d62c90d3a6b56
kumutha404 <144340522+kumutha404@users.noreply.github.com>
2023-09-07 04:25:08 -0700

    Add files via upload

---
+++ Unit 1 Challenges/Unit-1(P-2 LeapYear) (1).py
@@ -0,0 +1,13 @@
+def isLeapYear(Year):
+    if(Year % 4 == 0 and year % 100 !=0)or year % 400 ==0:
+        return True
+    else:
+        return False
+
+year=2020
+if isLeapYear(year):
+    print("{} is a leap year.".format(year))
+else:
+    print("{} is not a Leap year.".format(year))
+
+
